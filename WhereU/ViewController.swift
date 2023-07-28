5da526c8872cf339c5981268ab913c54e7cf7c05
EJLee1209 <dldmswo1209@gmail.com>
2023-07-28 19:17:45 +0900

    Initial Commit

---
+++ WhereU/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhereU
+//
+//  Created by 이은재 on 2023/07/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
